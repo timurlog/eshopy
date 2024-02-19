9ecfb60848b163e77c58bc4e17e97a9271adc5dd
timurlog <timur.logie@gmail.com>
2024-02-19 16:53:33 +0100

    added component SearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from "./components/Navbar/Navbar";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 export default function App() {
   return (
     <section>
-      <Navbar />
+      <SearchBar />
     </section>
   );
 }
