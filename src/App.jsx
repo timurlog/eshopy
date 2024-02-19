febe2a796196634f4e90f481860aaffcbe91ca23
timurlog <timur.logie@gmail.com>
2024-02-19 16:58:47 +0100

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar/Navbar";
 import SearchBar from "./components/SearchBar/SearchBar";
 
 export default function App() {
   return (
     <section>
+      <Navbar />
       <SearchBar />
     </section>
   );
