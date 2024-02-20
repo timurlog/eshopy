8d5a565df84bce3f6facb24a4792b9a887dc82cf
timurlog <timur.logie@gmail.com>
2024-02-20 11:59:09 +0100

    update fonts

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -47,7 +47,7 @@ export default function Cards(props) {
                   price: props.price,
                   stock: props.stock.toString()
                 })}
-           className="w-[110px] bg-black h-[40px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#58A4B0] before:to-[#58A4B0] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
+           className="w-[110px] bg-black h-[40px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#58A4B0] before:to-[#58A4B0] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 font-[garuteRegular] text-[#fff]">
             Buy
           </button>
         )}
